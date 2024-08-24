b63af1bc59c7e06f1e56638d9cfd5f5376135a65
MielAndMoon <dmestasa@gmail.com>
2024-08-24 11:40:02 -0500

    fix(database): migrate declarative_base to sqlalchemy 2.0

--- app/database.py
+++ app/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 URL_DATABASE = 'sqlite:///./finance.db'
 
