dbdb11257afdf519afe66a1c1271da536593d520
MielAndMoon <dmestasa@gmail.com>
2024-08-24 23:27:01 -0500

    feat(main): config the origins and middleware

--- app/main.py
+++ app/main.py
@@ -8,3 +8,12 @@ from fastapi.middleware.cors import CORSMiddleware
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    'http://localhost:5173'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins
+)
