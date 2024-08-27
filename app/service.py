8ec82e112462f9e1e633ffa098a7df0ef1d36918
MielAndMoon <dmestasa@gmail.com>
2024-08-27 08:52:48 -0500

    fix(service): change transaction model to transaction base

--- app/service.py
+++ app/service.py
@@ -1,14 +1,14 @@
 from app.models import Transaction
 from sqlalchemy.orm import Session
 
-from app.schemas import TransactionModel
+from app.schemas import TransactionBase
 
 
 def get_transactions(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Transaction).offset(skip).limit(limit).all()
 
 
-def insert_transaction(db: Session, transaction: TransactionModel):
+def insert_transaction(db: Session, transaction: TransactionBase):
     db_transaction = Transaction(**transaction.model_dump())
     db.add(db_transaction)
     db.commit()
